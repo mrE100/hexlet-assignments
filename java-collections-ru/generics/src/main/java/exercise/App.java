c34b019b8f439e9d89fe6055e8b75c7d46e155bc
@hexlet/cli <support@hexlet.io>
2022-12-05 18:54:07 +0800

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,7 +3,6 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
 
 // BEGIN
 public class App {
@@ -12,8 +11,10 @@ public class App {
         List<Map<String, String>> result = new ArrayList<>();
         for (Map currentMap : books) {
             List<String> keys = new ArrayList<>(where.keySet());
-            if (currentMap.get(keys.get(0)).equals(where.get(keys.get(0))) && currentMap.get(keys.get(1)).equals(where.get(keys.get(1)))) {
-                result.add(currentMap);
+            if (currentMap.get(keys.get(0)).equals(where.get(keys.get(0)))){
+                    if (currentMap.get(keys.get(1)).equals(where.get(keys.get(1)))){
+                    result.add(currentMap);
+                }
             }
         }
         return result;
