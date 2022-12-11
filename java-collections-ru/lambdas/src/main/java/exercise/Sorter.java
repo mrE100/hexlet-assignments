7a2a1a4354729422cc28809689ea84eea79970ff
@hexlet/cli <support@hexlet.io>
2022-12-11 21:50:02 +0800

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -10,6 +10,7 @@ public class Sorter {
         return users.stream()
                 .filter(user -> user.get("gender").equals("male"))
                 .sorted((user1, user2) -> user1.get("birthday").compareTo(user2.get("birthday")))
+                .map(user -> user.get("name"))
                 .toList();
     }
 }
