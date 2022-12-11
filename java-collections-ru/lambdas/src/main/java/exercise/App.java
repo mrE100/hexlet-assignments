5e57af7de0026e169cbdbe9acae00bdc04b8911b
@hexlet/cli <support@hexlet.io>
2022-12-11 21:55:28 +0800

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,6 +3,9 @@ package exercise;
 // BEGIN
 public class App {
     public static String[][] enlargeArrayImage(String[][] array) {
+        if (array.length == 0) {
+            return array;
+        }
         String[][] result = new String[array.length * 2][array[0].length * 2];
         for (int i = 0; i < array.length; i++) {
             int k = i * 2;
