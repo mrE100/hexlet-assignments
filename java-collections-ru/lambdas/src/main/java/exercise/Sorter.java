3bd7062f9b64d17ced300387a30ccdf799e495ec
@hexlet/cli <support@hexlet.io>
2022-12-11 21:45:19 +0800

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,10 +1,8 @@
 package exercise;
 
-import java.util.Comparator;
+
 import java.util.Map;
 import java.util.List;
-import java.time.LocalDate;
-import java.util.stream.Collectors;
 
 // BEGIN
 public class Sorter {
