a4854d9c5ba98aceac2d11ad8c9e74403e3bc179
@hexlet/cli <support@hexlet.io>
2022-12-13 22:21:16 +0800

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/test/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/test/java/exercise/App.java
@@ -2,13 +2,12 @@ package exercise;
 import java.util.List;
 
 public class App {
-
     public static String getForwardedVariables(String environment) {
         StringBuilder builder = new StringBuilder();
         List<String> strings = environment.split(",");
         for (String string : strings) {
             if (string.startsWith("X_FORWARDED_")) {
-                builder.append(string.replace("X_FORWARDED_", ""))
+                builder.append(string.replace("X_FORWARDED_", ""));
             }
         }
         return builder.toString();
