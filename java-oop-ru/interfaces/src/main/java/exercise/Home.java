3f4e117e1dd6413820aa4c91e53d8c545029d7c3
Sergei Ryabov <41112773+mrE100@users.noreply.github.com>
2023-03-16 16:01:09 +0300

    Update Home.java

--- java-oop-ru/interfaces/src/main/java/exercise/Home.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Home.java
@@ -1,5 +1,22 @@
 package exercise;
 
 // BEGIN
+public interface Home {
+    double getArea();
 
+    public default int compareTo(Home object2) {
+        if (this.getArea() > object2.getArea()) {
+            return 1;
+        }
+        if (this.getArea() == object2.getArea()) {
+            return 0;
+        }
+        if (this.getArea() < object2.getArea()) {
+            return -1;
+        }
+        return 555;
+    }
+
+
+}
 // END
