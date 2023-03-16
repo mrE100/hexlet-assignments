de83f3318f72d2468d41079e50e38b5e26d2dfcd
Sergei Ryabov <41112773+mrE100@users.noreply.github.com>
2023-03-16 16:00:41 +0300

    Update Flat.java

--- java-oop-ru/interfaces/src/main/java/exercise/Flat.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Flat.java
@@ -1,5 +1,23 @@
 package exercise;
 
 // BEGIN
+public class Flat implements exercise.Home {
+    private double area;
+    private double balconyArea;
+    private int floor;
 
+    public Flat(double area, double balconyArea, int floor) {
+        this.area = area;
+        this.balconyArea = balconyArea;
+        this.floor = floor;
+    }
+    public double getArea() {
+        return area + balconyArea;
+    }
+
+    @Override
+    public String toString() {
+        return "Квартира площадью " + getArea() + " метров на " + floor + " этаже";
+    }
+}
 // END
