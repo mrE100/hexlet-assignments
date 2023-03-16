335f079700b31dd77469fd290205398d56db013d
Sergei Ryabov <41112773+mrE100@users.noreply.github.com>
2023-03-16 16:00:16 +0300

    Update Cottage.java

--- java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
@@ -1,5 +1,21 @@
 package exercise;
 
 // BEGIN
+public class Cottage implements exercise.Home {
+    private final double area;
+    private final int floorCount;
 
+    public Cottage(double area, int floorCount) {
+        this.area = area;
+        this.floorCount = floorCount;
+    }
+    public double getArea() {
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return floorCount + " этажный коттедж площадью " + getArea() + " метров";
+    }
+}
 // END
