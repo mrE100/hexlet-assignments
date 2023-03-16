fcf58f4534ca50eec6da167d8c739bc05f506370
Sergei Ryabov <41112773+mrE100@users.noreply.github.com>
2023-03-16 15:59:48 +0300

    Update ReversedSequence.java

--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -1,5 +1,30 @@
 package exercise;
 
 // BEGIN
+public class ReversedSequence implements CharSequence {
+    private final CharSequence text;
 
+    public ReversedSequence(CharSequence newText) {
+        StringBuilder toPut = new StringBuilder();
+        for (int i = newText.length() - 1; i >= 0; i--) {
+            toPut.append(newText.charAt(i));
+        }
+        this.text = toPut;
+    }
+
+    @Override
+    public int length() {
+        return text.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return text.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return text.subSequence(start, end);
+    }
+}
 // END
