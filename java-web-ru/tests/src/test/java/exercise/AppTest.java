ddf901b8a2f6d0f9b49b1eb0e2071fae8762a0fe
Sergei <mre@mail.ru>
2023-05-25 22:12:56 +0800

    tests

--- java-web-ru/tests/src/test/java/exercise/AppTest.java
+++ java-web-ru/tests/src/test/java/exercise/AppTest.java
@@ -22,7 +22,13 @@ class AppTest {
     private static String baseUrl;
 
     // BEGIN
-    
+    @BeforeAll
+    static void beforeAll() {
+        app = App.getApp();
+        app.start();
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+    }
     // END
 
     // Между тестами база данных очищается
@@ -62,6 +68,70 @@ class AppTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testValid() {
+        HttpResponse response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", "testIvan")
+                .field("lastName", "Ivanov")
+                .field("email", "mail@mail.com")
+                .field("password", "12345")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(302);
+        User DBUser = new QUser()
+                .firstName.equalTo("testIvan")
+                .findOne();
+        assertThat(DBUser).isNotNull();
+        assertThat(DBUser.getFirstName()).isEqualTo("testIvan");
+        assertThat(DBUser.getLastName()).isEqualTo("Ivanov");
+        assertThat(DBUser.getEmail()).isEqualTo("mail@mail.com");
+        assertThat(DBUser.getPassword()).isEqualTo("12345");
+    }
+
+    @Test
+    void notValid() {
+        HttpResponse response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", "testIvan")
+                .field("lastName", "Ivanov")
+                .field("email", "mail@mail.com")
+                .field("password", "123")
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(422);
+        User DBUser = new QUser()
+                .firstName.equalTo("testIvan")
+                .findOne();
+        assertThat(DBUser).isNull();
+        response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", "testIvan")
+                .field("lastName", "Ivanov")
+                .field("email", "mail")
+                .field("password", "12345")
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(422);
+        DBUser = new QUser()
+                .firstName.equalTo("testIvan")
+                .findOne();
+        assertThat(DBUser).isNull();
+        response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", "testIvan")
+                .field("lastName", "")
+                .field("email", "mail@mail.com")
+                .field("password", "12345")
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(422);
+        DBUser = new QUser()
+                .firstName.equalTo("testIvan")
+                .findOne();
+        assertThat(DBUser).isNull();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        app.stop();
+    }
     // END
 }
