efda360ba40db340bed8415fefc149f06485fe7a
Sergei <mre@mail.ru>
2023-05-25 20:20:33 +0800

    validation

--- java-web-ru/validation/src/main/java/exercise/controllers/UserController.java
+++ java-web-ru/validation/src/main/java/exercise/controllers/UserController.java
@@ -3,6 +3,8 @@ package exercise.controllers;
 import io.javalin.http.Handler;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+
 import io.javalin.validation.Validator;
 import io.javalin.validation.ValidationError;
 import io.javalin.validation.JavalinValidation;
@@ -51,7 +53,42 @@ public final class UserController {
 
     public static Handler createUser = ctx -> {
         // BEGIN
-        
+        String firstName = ctx.formParam("firstName");
+        String lastName = ctx.formParam("lastName");
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");;
+
+        Validator<String> firstNameValidator = ctx.formParamAsClass("firstName", String.class)
+                .check(it -> !it.isEmpty(), "Имя не должно быть пустым");
+        Validator<String> lastNameValidator = ctx.formParamAsClass("lastName", String.class)
+                .check(it -> !it.isEmpty(), "Имя не должно быть пустым");
+        Validator<String> emailValidator = ctx.formParamAsClass("email", String.class)
+                .check(it -> EmailValidator.getInstance().isValid(it), "Должен быть валидным Email");
+        Validator<String> passwordValidator = ctx.formParamAsClass("password", String.class)
+                .check(it -> StringUtils.isNumeric(it), "Должен быть не короче 4 символов и содержать только цифры")
+                .check(it -> StringUtils.length(it) > 3, "Должен быть не короче 4 символов и содержать только цифры");
+
+        Map<String, List<ValidationError<? extends Object>>> errors = JavalinValidation.collectErrors(
+                firstNameValidator,
+                lastNameValidator,
+                emailValidator,
+                passwordValidator);
+
+        if (!errors.isEmpty()) {
+            // Устанавливаем код ответа
+            ctx.status(422);
+            // Передаем ошибки и данные компании
+            ctx.attribute("errors", errors);
+            User invalidUser = new User(firstName, lastName, email, password);
+            ctx.attribute("user", invalidUser);
+            ctx.render("users/new.html");
+            return;
+        }
+
+        User user = new User(firstName, lastName, email, password);
+        user.save();
+
+        ctx.redirect("/users");
         // END
     };
 }
