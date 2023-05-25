f08f73f95f3583d6e481106787cfd4dc1428129a
@hexlet/cli <support@hexlet.io>
2023-05-25 19:56:15 +0800

    submit java-web-ru/validation

---
+++ java-web-ru/validation/src/main/java/exercise/controllers/UserController.java
@@ -0,0 +1,57 @@
+package exercise.controllers;
+
+import io.javalin.http.Handler;
+import java.util.List;
+import java.util.Map;
+import io.javalin.validation.Validator;
+import io.javalin.validation.ValidationError;
+import io.javalin.validation.JavalinValidation;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.lang3.StringUtils;
+import io.javalin.http.Context;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+
+public final class UserController {
+
+    private static void removeFlashMessage(Context ctx) {
+        ctx.sessionAttribute("flash", null);
+    }
+
+    public static Handler listUsers = ctx -> {
+
+        List<User> users = new QUser()
+            .orderBy()
+                .id.asc()
+            .findList();
+
+        ctx.attribute("users", users);
+        ctx.render("users/index.html");
+        removeFlashMessage(ctx);
+    };
+
+    public static Handler showUser = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        User user = new QUser()
+            .id.equalTo(id)
+            .findOne();
+
+        ctx.attribute("user", user);
+        ctx.render("users/show.html");
+    };
+
+    public static Handler newUser = ctx -> {
+
+        ctx.attribute("errors", Map.of());
+        ctx.attribute("user", Map.of());
+        ctx.render("users/new.html");
+    };
+
+    public static Handler createUser = ctx -> {
+        // BEGIN
+        
+        // END
+    };
+}
