88b6e061ce4f9134fea707ba458384281e2f2285
Sergei <mre@mail.ru>
2023-07-18 17:46:55 +0800

    welcome

--- java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -2,7 +2,6 @@ package exercise;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
@@ -15,9 +14,8 @@ public class WelcomeController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
-    public String getFoos(@RequestParam(defaultValue = "World") String id) {
-        return "Hello, " + id + "!";
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
 // END
