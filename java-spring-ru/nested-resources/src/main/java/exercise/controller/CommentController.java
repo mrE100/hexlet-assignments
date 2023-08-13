9cc03f8b06633111401d0401833f3d782e4c0d5e
@hexlet/cli <support@hexlet.io>
2023-08-13 22:05:13 +0800

    submit java-spring-ru/nested-resources

---
+++ java-spring-ru/nested-resources/src/main/java/exercise/controller/CommentController.java
@@ -0,0 +1,32 @@
+package exercise.controller;
+
+import exercise.dto.CommentDto;
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+
+@RestController
+@RequestMapping("/posts")
+@AllArgsConstructor
+public class CommentController {
+
+    private final CommentRepository commentRepository;
+
+    private final PostRepository postRepository;
+
+    // BEGIN
+    
+    // END
+}
